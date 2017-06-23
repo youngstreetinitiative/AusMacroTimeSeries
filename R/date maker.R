88a3fd94d644b9b5bc57d744f98bf3ad6cc7771b
rosseji <rosseireland@gmail.com>
2017-06-23 16:29:02 +1000

    im hoping it's fixed!!

--- R/date maker.R
+++ R/date maker.R
@@ -17,11 +17,12 @@ date_maker_loop <- function(df) {
 
 date_maker_list <- function(df_list) {
 
-  df_list %>%
+  df_list <- df_list %>%
     map(~mutate(.,
                 Date = as.Date(Date, format = "%d/%m/%Y")))
 
-  invisible(lapply(df_list, function(x) {nm <- deparse(substitute(x)); assign(nm,x,.GlobalEnv)}))
+  lapply(names(df_list), function(x) {assign(x, df_list[[x]],.GlobalEnv)})
+
 
   df_list
 }
